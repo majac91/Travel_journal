b4564da4bf6f0c5a74b5e765e83ae110331eb60f
Maja <majacvetkovic@outlook.com>
2020-12-12 15:21:57 +0100

    Fix header img

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import "./styles/index.scss";
+import img from "./images/sienna.jpg";
+import "./icons/font/_flaticon.scss";
+
 import destinationsList from "./js/store.js";
 import moduleDestinations from "./js/modules/list.js";
 import addNewCity from "./js/modules/add_destination.js";
 import events from "./js/modules/pubsub.js";
 moduleDestinations.init();
 addNewCity.init();
+
+const headerImg = document.querySelector(".header__img");
+headerImg.src = img;
